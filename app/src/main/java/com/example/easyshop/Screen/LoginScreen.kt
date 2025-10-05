3a499644e0aaef94265233c01ae04142473aca56
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-06 00:11:06 +0530

    LoginScreen.kt commit

---
+++ app/src/main/java/com/example/easyshop/Screen/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.example.easyshop.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+    
+}
