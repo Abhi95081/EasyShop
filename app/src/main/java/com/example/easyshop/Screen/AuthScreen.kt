2e8438165ecf9bb23ed16dc54804b99fca369217
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-08 22:45:36 +0530

    SignupScreen.kt commit

--- app/src/main/java/com/example/easyshop/Screen/AuthScreen.kt
+++ app/src/main/java/com/example/easyshop/Screen/AuthScreen.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -70,10 +72,15 @@ fun AuthScreen(modifier: Modifier, navController: NavHostController) {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedButton (onClick = {
-            navController.navigate("signup")
-        },
-            modifier = Modifier.fillMaxWidth().height(60.dp)
+        OutlinedButton(
+            onClick = { navController.navigate("signup") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White
+            )
         ) {
             Text("Sign Up")
         }
