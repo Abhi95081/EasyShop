bf98f6064437d2ec59e39946d5a098236ec028d2
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-18 21:47:51 +0530

    SignupScreen.kt commit

--- app/src/main/java/com/example/easyshop/Screen/SignupScreen.kt
+++ app/src/main/java/com/example/easyshop/Screen/SignupScreen.kt
@@ -1,9 +1,132 @@
 package com.example.easyshop.Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.easyshop.R
 
 @Composable
 fun SignupScreen(modifier: Modifier = Modifier) {
+
+    var email by remember{
+        mutableStateOf("")
+    }
+
+
+    var name by remember{
+        mutableStateOf("")
+    }
+
+    var password by remember{
+        mutableStateOf("")
+    }
+
+    Column(
+        modifier= Modifier.fillMaxSize()
+            .padding(32.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Text("Hello there!",
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontWeight  = FontWeight.Bold,
+                fontFamily = FontFamily.Monospace,
+            )
+        )
+        Spacer(modifier.height(10.dp))
+
+        Text("Create an account",
+            style = TextStyle(
+                fontSize = 22.sp
+            )
+        )
+        Spacer(modifier.height(10.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.banner_user),
+            contentDescription = "Banner_user",
+            modifier = Modifier.fillMaxWidth()
+                .height(200.dp)
+        )
+
+        Spacer(modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email ,onValueChange ={
+            email = it
+        },
+            label = {
+                Text("Email Address")
+            },
+            modifier = Modifier.fillMaxWidth()
+            )
+
+        Spacer(modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = name ,onValueChange ={
+                name = it
+            },
+            label = {
+                Text("Full Name")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier.height(10.dp))
+
+        OutlinedTextField(
+            value = password ,onValueChange ={
+                password = it
+            },
+            label = {
+                Text("Password")
+            },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(onClick = {
+
+        },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Text("SignUp", fontSize = 22.sp)
+        }
+
+
+    }
     
 }
