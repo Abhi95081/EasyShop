92d8ecbb5098cbd8094c22ff359f149dea8a1d4d
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-21 16:43:34 +0530

    HeaderView.kt commit

---
+++ app/src/main/java/com/example/easyshop/components/HeaderView.kt
@@ -0,0 +1,60 @@
+package com.example.easyshop.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import java.security.AllPermission
+
+@Composable
+fun HeaderView(modifier: Modifier = Modifier) {
+
+    var name by remember { mutableStateOf("") }
+
+    LaunchedEffect(Unit) {
+        Firebase.firestore.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid!!)
+            .get().addOnCompleteListener(){
+                name = it.result?.get("name").toString().split(" ")[0]
+            }
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Column{
+            Text("Welcome Back")
+            Text(name,style = TextStyle(
+                fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+                fontWeight = FontWeight.Bold
+            ))
+        }
+        IconButton(onClick = {}) {
+            Icon(imageVector = Icons.Default.Search, contentDescription = "Notifications")
+        }
+    }
+
+
+    
+}
