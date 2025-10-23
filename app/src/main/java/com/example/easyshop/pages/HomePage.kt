ac97d16bf44bd25a8118410560517e5680ffb719
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-23 13:10:48 +0530

    homeview commit

--- app/src/main/java/com/example/easyshop/pages/HomePage.kt
+++ app/src/main/java/com/example/easyshop/pages/HomePage.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -15,10 +17,10 @@ import com.example.easyshop.components.HeaderView
 fun HomePage(modifier: Modifier = Modifier) {
 
     Column(
-        modifier = Modifier.fillMaxSize().padding(16.dp)
+        modifier = Modifier.fillMaxSize().padding(20.dp)
+        .verticalScroll(rememberScrollState())
     ){
         HeaderView(modifier)
-        Spacer(modifier= Modifier.height(10.dp))
         BannerView(modifier = Modifier.height(200.dp))
     }
     
