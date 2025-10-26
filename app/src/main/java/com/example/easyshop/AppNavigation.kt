b836c355ca469e3e59a7ebec9a6cbdbc443345b2
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-26 13:31:50 +0530

    AppNavigation.kt commit

--- app/src/main/java/com/example/easyshop/AppNavigation.kt
+++ app/src/main/java/com/example/easyshop/AppNavigation.kt
@@ -2,6 +2,7 @@ package com.example.easyshop
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -9,6 +10,7 @@ import com.example.easyshop.Screen.AuthScreen
 import com.example.easyshop.Screen.HomeScreen
 import com.example.easyshop.Screen.LoginScreen
 import com.example.easyshop.Screen.SignupScreen
+import com.example.easyshop.pages.CategoryProductPage
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
@@ -16,6 +18,7 @@ import com.google.firebase.auth.auth
 fun AppNavigation(modifier: Modifier = Modifier) {
 
     val navController = rememberNavController()
+    GlobalNavigation.navController = navController
 
     val isLoggedIn = Firebase.auth.currentUser!=null
     val firstpage = if(isLoggedIn) "home" else "auth"
@@ -37,6 +40,14 @@ fun AppNavigation(modifier: Modifier = Modifier) {
         composable("home"){
             HomeScreen(modifier,navController)
         }
+
+        composable("category-products/{categoryId}"){
+            val categoryId = it.arguments?.getString("categoryId")
+            CategoryProductPage(modifier,categoryId?:"")
+        }
     }
-    
+}
+
+object GlobalNavigation{
+    lateinit var navController: NavHostController
 }
