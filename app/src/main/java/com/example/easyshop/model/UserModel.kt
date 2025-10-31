d1f61150b7a2c7806fa9071c0dd1a0978095c994
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-31 14:14:10 +0530

    Checkout and razorpay commit

--- app/src/main/java/com/example/easyshop/model/UserModel.kt
+++ app/src/main/java/com/example/easyshop/model/UserModel.kt
@@ -3,5 +3,6 @@ package com.example.easyshop.model
 data class UserModel (
     val name : String,
     val email : String,
-    val uid : String
+    val uid : String,
+    val cartItem : Map<String,Long> = emptyMap()
 )
