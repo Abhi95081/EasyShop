8dfc9c7b4c75425d390b155bc4bb4406c89ff28d
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-10-31 14:13:52 +0530

    Checkout and razorpay commit

--- app/src/main/java/com/example/easyshop/MainActivity.kt
+++ app/src/main/java/com/example/easyshop/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.easyshop
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.easyshop.ui.theme.EasyShopTheme
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), PaymentResultListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             EasyShopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -25,5 +26,14 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
+    // ✅ Payment success callback
+    override fun onPaymentSuccess(p0: String?) {
+        AppUtil.showToast(this,"✅ Payment Successful")
+    }
+
+    // ✅ Payment failure callback
+    override fun onPaymentError(p0: Int, p1: String?) {
+        AppUtil.showToast(this,"❌ Payment Failed")
+    }
+}
